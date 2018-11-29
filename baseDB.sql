e914ae39e4d7af10beaa134c1d96997533ff016f
Marcus Crisostomo <marcus.crisostomo@yahoo.com.br>
2018-11-29 18:20:47 -0200

    Fix Auto-Increment option in some database tables

--- baseDB.sql
+++ baseDB.sql
@@ -1,8 +1,8 @@
 -- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
 --
--- Host: us-cdbr-iron-east-01.cleardb.net    Database: heroku_73e4e22d73e7b8a
+-- Host: localhost    Database: ps2onlinedev
 -- ------------------------------------------------------
--- Server version	5.5.56-log
+-- Server version	5.5.5-10.1.34-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -26,17 +26,8 @@ CREATE TABLE `tb_category` (
   `idCategory` int(11) NOT NULL AUTO_INCREMENT,
   `categoryName` varchar(45) NOT NULL,
   PRIMARY KEY (`idCategory`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tb_category`
---
-
-LOCK TABLES `tb_category` WRITE;
-/*!40000 ALTER TABLE `tb_category` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tb_category` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `tb_color_status`
@@ -46,21 +37,12 @@ DROP TABLE IF EXISTS `tb_color_status`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `tb_color_status` (
-  `idStatus` int(11) NOT NULL,
+  `idStatus` int(11) NOT NULL AUTO_INCREMENT,
   `typeStatus` varchar(100) NOT NULL,
   `colorStatus` varchar(7) DEFAULT NULL,
   PRIMARY KEY (`idStatus`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tb_color_status`
---
-
-LOCK TABLES `tb_color_status` WRITE;
-/*!40000 ALTER TABLE `tb_color_status` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tb_color_status` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `tb_dns`
@@ -76,17 +58,8 @@ CREATE TABLE `tb_dns` (
   `dnsSecondary` varchar(45) NOT NULL,
   `dnsWeb` varchar(100) NOT NULL,
   PRIMARY KEY (`idDNS`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tb_dns`
---
-
-LOCK TABLES `tb_dns` WRITE;
-/*!40000 ALTER TABLE `tb_dns` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tb_dns` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `tb_game`
@@ -114,24 +87,13 @@ CREATE TABLE `tb_game` (
   KEY `FK_DNS_idx` (`dns1`),
   KEY `FK_DNS2_idx` (`dns2`),
   KEY `FK_ORIGINAL_HOST_idx` (`originalHost`),
-  KEY `FK_STATUS_idx` (`status`),
-  CONSTRAINT `FK_STATUS` FOREIGN KEY (`status`) REFERENCES `tb_color_status` (`idStatus`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `FK_DNS1` FOREIGN KEY (`dns1`) REFERENCES `tb_dns` (`idDNS`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `FK_DNS2` FOREIGN KEY (`dns2`) REFERENCES `tb_dns` (`idDNS`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `FK_GAME_CATEGORY` FOREIGN KEY (`gameCategory`) REFERENCES `tb_category` (`idCategory`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `FK_GAME_REGION` FOREIGN KEY (`gameRegion`) REFERENCES `tb_region` (`idRegion`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `FK_ORIGINAL_HOST` FOREIGN KEY (`originalHost`) REFERENCES `tb_original_host` (`idHost`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tb_game`
---
-
-LOCK TABLES `tb_game` WRITE;
-/*!40000 ALTER TABLE `tb_game` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tb_game` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `tb_game_image`
@@ -141,7 +103,7 @@ DROP TABLE IF EXISTS `tb_game_image`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `tb_game_image` (
-  `idImage` int(11) NOT NULL,
+  `idImage` int(11) NOT NULL AUTO_INCREMENT,
   `gameCoverURL` varchar(45) DEFAULT NULL,
   `screenshot1` varchar(45) DEFAULT NULL,
   `screenshot2` varchar(45) DEFAULT NULL,
@@ -156,15 +118,6 @@ CREATE TABLE `tb_game_image` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `tb_game_image`
---
-
-LOCK TABLES `tb_game_image` WRITE;
-/*!40000 ALTER TABLE `tb_game_image` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tb_game_image` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `tb_game_review`
 --
 
@@ -172,7 +125,7 @@ DROP TABLE IF EXISTS `tb_game_review`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `tb_game_review` (
-  `idReview` int(11) NOT NULL,
+  `idReview` int(11) NOT NULL AUTO_INCREMENT,
   `reviewEN` longtext,
   `reviewPT` longtext,
   `reviewES` longtext,
@@ -182,15 +135,6 @@ CREATE TABLE `tb_game_review` (
   CONSTRAINT `FK_ID_GAME_REVIEW` FOREIGN KEY (`idGame`) REFERENCES `tb_game` (`idGame`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tb_game_review`
---
-
-LOCK TABLES `tb_game_review` WRITE;
-/*!40000 ALTER TABLE `tb_game_review` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tb_game_review` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `tb_original_host`
@@ -203,17 +147,8 @@ CREATE TABLE `tb_original_host` (
   `idHost` int(11) NOT NULL AUTO_INCREMENT,
   `hostName` varchar(45) NOT NULL,
   PRIMARY KEY (`idHost`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tb_original_host`
---
-
-LOCK TABLES `tb_original_host` WRITE;
-/*!40000 ALTER TABLE `tb_original_host` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tb_original_host` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `tb_region`
@@ -227,17 +162,8 @@ CREATE TABLE `tb_region` (
   `regionName` varchar(10) NOT NULL,
   `regionAlias` varchar(3) NOT NULL,
   PRIMARY KEY (`idRegion`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tb_region`
---
-
-LOCK TABLES `tb_region` WRITE;
-/*!40000 ALTER TABLE `tb_region` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tb_region` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -248,4 +174,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-11-27 14:50:29
+-- Dump completed on 2018-11-29 18:13:57
